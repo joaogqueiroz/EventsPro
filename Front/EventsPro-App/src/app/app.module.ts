bf7c2cff8a347890015cc4cebede1bf1f63a9240
joaogqueiroz <joaogilberto350@hotmail.com>
2023-08-16 18:07:53 -0300

    feat: including toastr, spinning, tooltip, dropdown

--- Front/EventsPro-App/src/app/app.module.ts
+++ Front/EventsPro-App/src/app/app.module.ts
@@ -1,17 +1,26 @@
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import {HttpClientModule} from '@angular/common/http'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { EventsComponent } from './events/events.component';
-import { SpeakersComponent } from './speakers/speakers.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NavComponent } from './nav/nav.component';
 
 import { CollapseModule } from 'ngx-bootstrap/collapse';
+import { TooltipModule } from 'ngx-bootstrap/tooltip';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { ModalModule } from 'ngx-bootstrap/modal';
+import { ToastrModule } from 'ngx-toastr';
+import { NgxSpinnerModule } from 'ngx-spinner';
+
+
+import { EventsComponent } from './events/events.component';
+import { NavComponent } from './nav/nav.component';
+import { SpeakersComponent } from './speakers/speakers.component';
+
 import { EventService } from './Services/event.service';
+
 import { DateTimeFormatPipe } from './helpers/date-time-format.pipe';
 
 @NgModule({
@@ -28,11 +37,22 @@ import { DateTimeFormatPipe } from './helpers/date-time-format.pipe';
     HttpClientModule,
     BrowserAnimationsModule,
     CollapseModule.forRoot(),
+    TooltipModule.forRoot(),
+    BsDropdownModule.forRoot(),
+    ModalModule.forRoot(),
+    ToastrModule.forRoot({
+      timeOut: 3000,
+      positionClass: 'toast-bottom-right',
+      preventDuplicates: true,
+      progressBar: true,
+    }),
+    NgxSpinnerModule,
     FormsModule
   ],
   providers: [
     EventService
   ],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class AppModule { }
--- Front/EventsPro-App/src/app/events/events.component.ts
+++ Front/EventsPro-App/src/app/events/events.component.ts
@@ -1,6 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, TemplateRef } from '@angular/core';
 import {EventService} from './../Services/event.service';
 import { Event } from "../models/event";
+import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
+import { ToastrService } from 'ngx-toastr';
+import { NgxSpinnerService } from 'ngx-spinner';
 
 @Component({
   selector: 'app-events',
@@ -14,6 +17,7 @@ export class EventsComponent implements OnInit {
   marginImg = 2;
   showImgState = true;
   private filteredList: string = '';
+  modalRef?: BsModalRef;
 
   public get filterList(): string {
     return this.filteredList;
@@ -33,24 +37,51 @@ export class EventsComponent implements OnInit {
         event.place?.toLowerCase().indexOf(filterListBy) !== -1
     );
   }
-  constructor(private EventService: EventService) {}
+  constructor(
+    private EventService: EventService,
+    private modalService: BsModalService,
+    private toastrService: ToastrService,
+    private spinner: NgxSpinnerService,
+    ) {}
 
   public ngOnInit(): void {
+    this.spinner.show();
     this.getEvents();
+
   }
 
   public showImage(): void {
     this.showImgState = !this.showImgState;
   }
 
+  openModal(template: TemplateRef<any>) {
+    this.modalRef = this.modalService.show(template, {class: 'modal-sm'});
+  }
+ 
+  confirm(): void {
+    this.modalRef?.hide();
+    this.toastrService.success('Event deleted successfully', 'Deleted')
+  }
+ 
+  decline(): void {
+    this.modalRef?.hide();
+  }
+
   public getEvents(): void {
-    this.EventService.getEvents().subscribe(
-      (eventsResponse : Event[]) => {
+    this.EventService.getEvents().subscribe({
+      next: (eventsResponse : Event[]) => {
         this.events = eventsResponse;
         console.log(this.events);
         this.filtredEvents = this.events;
       },
-      (error) => console.log(error)
-    );
+      error: (error: any) => {
+        console.log(error);
+        this.spinner.hide();
+        this.toastrService.error('Error loading events','Error');
+      },
+      complete: () => {
+        this.spinner.hide();
+      }
+    });
   }
 }
